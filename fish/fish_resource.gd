fd8965ecf87a7f26b4fc39fb46de035fb260b126
Dennis <2x.denn@gmail.com>
2022-10-13 21:30:53 -0400

    2 new fish, FishPick works
    to add new fish add a resource file of fishStats and connect the glb file of the fish mesh to it, then reimport the JSON spreadsheet file to godot

--- fish/fish_resource.gd
+++ fish/fish_resource.gd
@@ -2,7 +2,12 @@ extends Resource
 class_name FishStats
 
 @export var fish_name : String
-@export var fish_mesh : Node3D
-@export var fish_rarity : int   #1 is common, 2 is uncommon, 3 is rare
-@export var fish_scale_min : float
-@export var fish_scale_max : float
+@export var fish_node : PackedScene
+#@export var fish_rarity : String  
+#@export var fish_scale_min : float
+#@export var fish_scale_max : float
+
+func getName():
+	return fish_name
+func getNode():
+	return fish_node
--- scenes/PickFish.gd
+++ scenes/PickFish.gd
@@ -5,60 +5,111 @@ extends Node
 #giving you a better chance at picking a higher number
 @export var luck: int
 
+@onready var fish_name : String
+@onready var rarity : String
 @onready var fish_min : float
 @onready var fish_max : float
-@onready var rarity : int
+@onready var fish_id : int
+
+@onready var fishSpawnPoint = get_node("%FishSpawn")
+
+@onready var fishData : Dictionary
 
 @onready var rng_size = 0
 
-var list = ["Rudd",1,"egg",3,"dog"]
 
+func LoadFishData(FilePath):
+	
+	var DataFile = File.new()
+	DataFile.open(FilePath, File.READ)
+	var DataJSon = JSON.new()
+	DataJSon = DataJSon.parse_string(DataFile.get_as_text())
+	DataFile.close()
+	return DataJSon
 
 func _ready():
 	
-	var arraySize = list.size()
+	fishData = LoadFishData("res://json/FishTable - Sheet1.json")
+	setFish(pickFish())
+	createFish()
 	
 	
+	print("Fish Name: " + fish_name)
+	print("Fish Rarity: " + rarity)
+	print("Fish Min: " + str(fish_min))
+	print("Fish Max: " + str(fish_max))
+	print("Fish ID: " + str(fish_id))
 
-#needs to return a String with the fishes name after it is called
-#has to pick a rarity and then a fish in that rarity class
+#creates the fish given the parameters with a node3d and gives it a mesh
+func createFish():
+
+	var fish_resource = load("res://fish.tres/" + fish_name + ".tres")
+	print(fish_resource.getName())
+	#print(fish_resource.getNode())
+	#var fishNode = fish_resource.getNode().instantiate()
+	fishSpawnPoint.add_child(fish_resource.getNode().instantiate())
+
+
+#returns a String with a fishes index in it
 func pickFish():
 	
 	var rng = RandomNumberGenerator.new()
 	rng.randomize()
 	var rng_num = int(rng.randf_range(0 + luck, 100))
-	#print("FISH PICK NUMBER: " , rng_num)
+	print("FISH PICK NUMBER: " , rng_num)
 	
 	#common
 	if(rng_num > 0 && rng_num < 70):
-		#has to read through array of fish names with the common rarity and
-		#select one at random
-		pass
+		#creates an array with the index's of all the common fish and selects
+		#one of them at random, returning a string with its name
+		var commonFish : Array
+		for i in fishData.size():
+			i += 1
+			if(fishData[str(i)]["RarityTag"] == "Common"):
+				commonFish += [i]
+		#returns a random common fish index
+		return commonFish[randi() % commonFish.size()]
 	#uncommon
 	if(rng_num > 70 && rng_num < 90):
-		#has to read through array of fish names with the uncommon rarity and
-		#select one at random
-		pass
+		var uncommonFish : Array
+		for i in fishData.size():
+			i += 1
+			if(fishData[str(i)]["RarityTag"] == "Uncommon"):
+				uncommonFish += [i]
+		return uncommonFish[randi() % uncommonFish.size()]
 	#rare
 	if(rng_num > 90 && rng_num < 100):
-		#has to read through array of fish names with the uncommon rarity and
-		#select one at random
-		pass
-	
-	return "rudd"
+		var rareFish : Array
+		for i in fishData.size():
+			i += 1
+			if(fishData[str(i)]["RarityTag"] == "Rare"):
+				rareFish += [i]
+		return rareFish[randi() % rareFish.size()]
 
+func setFish(index : int):
+	
+	fish_id = index
+	fish_name = fishData[str(index)]["FishName"]
+	fish_min = fishData[str(index)]["FishScaleMin"]
+	fish_max = fishData[str(index)]["FishScaleMax"]
+	rarity = fishData[str(index)]["RarityTag"]
 
 #has to set a random fish size between the fishes min size and max size
 #has to read in this data from the fish type
-func setFishSize(fish, fish_size_min, fish_size_max):
+func setFishSize(fish):
+	
+	var fish_size_min = fishData[fish]["FishScaleMin"]
+	var fish_size_max = fishData[fish]["FishScaleMax"]
 	var rng = RandomNumberGenerator.new()
 	rng.randomize()
 	rng_size = rng.randf_range(fish_size_min, fish_size_max)
-	fish.scale = rng_size
+	#fish.scale = rng_size
+	
+	print(fishData[fish])
 
 #has to return the size of the fish after it has been set in setFishSize 
 func getFishSize():
 	return rng_size
 
 func getFishRarity():
-	return 0
+	return rarity
